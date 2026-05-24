d344e3de0180d1b4b3e21cb7f6d3620e7c2bc843
Nadia Nur Nisrina <nadia.nurnisrina.2004@gmail.com>
2026-05-24 20:52:43 +0700

    feat: scaffold WebStory1 modular components

---
+++ src/pages/WebStory1/components/HusnaViews.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './HusnaViews.css';
+
+export const View5 = () => {
+  return (
+    <section className="webstory-view view-5">
+      <h2>View 5 (Inti Lapangan) - Husna</h2>
+      {/* TODO: Add inti lapangan content and animations here */}
+    </section>
+  );
+};
+
+export const View7 = () => {
+  return (
+    <section className="webstory-view view-7">
+      <h2>View 7 (Tantangan/Parallax) - Husna</h2>
+      {/* TODO: Add tantangan content and animations here */}
+    </section>
+  );
+};
