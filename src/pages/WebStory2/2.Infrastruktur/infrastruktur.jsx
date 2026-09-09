00096ade2148af83a33bd48cf63c26008c0114bf
viersss <vieryrraditio@gmail.com>
2026-09-09 10:59:59 +0700

    coba acc ini

--- src/pages/WebStory2/2.Infrastruktur/infrastruktur.jsx
+++ src/pages/WebStory2/2.Infrastruktur/infrastruktur.jsx
@@ -1,3 +1,7 @@
+/* ─────────────────────────────────────────────────────────────────
+   File Infrastruktur yang Baru (2.0)
+───────────────────────────────────────────────────────────────── */
+
 import React, { useEffect, useRef, useState, useCallback } from 'react';
 import insights from '../insight.json';
 import { MapContainer, TileLayer, CircleMarker, Popup, useMap } from 'react-leaflet';
