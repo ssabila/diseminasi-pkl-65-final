432accbdc5ba8525df8415b22d83dc0acfbc7e19
viersss <vieryrraditio@gmail.com>
2026-09-09 11:49:59 +0700

    ini ya

--- src/pages/WebStory2/2.Infrastruktur/infrastruktur.jsx
+++ src/pages/WebStory2/2.Infrastruktur/infrastruktur.jsx
@@ -1,3 +1,5 @@
+
+
 import React, { useEffect, useRef, useState, useCallback } from 'react';
 import insights from '../insight.json';
 import { MapContainer, TileLayer, CircleMarker, Popup, useMap } from 'react-leaflet';
